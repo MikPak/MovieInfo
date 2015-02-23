6be29abee68ac3db4d6245b58eadb95408862b24
Mpak <h8699@student.jamk.fi>
2015-02-23 19:11:51 +0200

    todo-list

--- MovieInfo/src/main.java
+++ MovieInfo/src/main.java
@@ -5,17 +5,18 @@ MovieInfo is a simple movie-library tool for retrieving movie information from
 Internet Movie Database (IMDB.com) and showing this info in neat and simple GUI.
 
 Complete features:
-
-Partially complete features:
 - Retrieve movies from a given folder (MPa)
 - Parse folder names for making a query to OMDb. (MPa)
 - Method for connecting to OMDb and passing folder name as movie name. (MPa)
 - Parsing OMDb response & showing it in the client. (MPa)
 
-TODO:
+Partially complete features:
 - Make a first GUI.
+
+TODO:
 - Make interactive GUI so it works with OMDb
-- Store fetched movie information in .xml or database and load info from there.
+- Store fetched movie information in .xml or database and load info from there,
+  instead of making query to OMDb every time.
 
 
 Authors: Mikko Pakkanen, Mikko Tella 
