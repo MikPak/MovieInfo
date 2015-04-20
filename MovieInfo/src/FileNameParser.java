c4a58601ffc2047de8fd324bde69be498400cd6c
Mpak <h8699@student.jamk.fi>
2015-04-20 22:48:34 +0300

    Jlist fixauksia

--- MovieInfo/src/FileNameParser.java
+++ MovieInfo/src/FileNameParser.java
@@ -16,11 +16,8 @@ import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
